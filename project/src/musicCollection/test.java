ed4db70b3a4df4bef142f08c976122083be75bed
DimitrisTem <tem1654@tem.uoc.gr>
2014-01-19 15:58:44 +0200

    Library JFugue

--- project/src/musicCollection/test.java
+++ project/src/musicCollection/test.java
@@ -7,12 +7,15 @@ import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Sequencer;
 
+import org.jfugue.Player;
+
 public class test {
 	public static void main(String[] args) throws InvalidMidiDataException, IOException, MidiUnavailableException{
 		MusicCollectionPlayer pla = new MusicCollectionPlayer("C:/Users/Dimitris/Downloads/testHY252.mid");
 		
 		pla.Play(pla.sequencer,"C:/Users/Dimitris/Downloads/testHY252.mid");
-		pla.Stop(pla.sequencer);
 		
+		Player playerw = new Player();
+		playerw.play("A A A C C C D D ");
 	}
 }
